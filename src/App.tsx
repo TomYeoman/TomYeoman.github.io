5b34b897956cbd4220d5777ecf1086bb6ed1e7fd
Tom <tomyeoman.github@fastmail.com>
2020-11-20 11:08:57 +0000

    Temp remove router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 import UserProfile from "./features/user/UserProfile";
 
 const App = () => (
 
-  <Router>
-    <Route path="/" component={UserProfile} />
-  </Router>
+  <UserProfile/>
+  // <Router>
+  //   <Route path="/" component={UserProfile} />
+  // </Router>
 );
 
 export default App;
