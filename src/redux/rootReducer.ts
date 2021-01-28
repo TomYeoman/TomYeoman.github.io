9a065413de23a0577c02b90d6acc883ecfbfe82b
Tom Yeoman <tom.yeoman@live.co.uk>
2021-01-28 16:53:22 +0000

    Convert from CRA to gatsby

--- src/redux/rootReducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit'
-import userSlice from '../features/user/userReducer'
-
-const rootReducer = combineReducers({userSlice})
-
-export type RootState = ReturnType<typeof rootReducer>
-
-export default rootReducer
